a6f69472fea38747fbeb3411d06eea5c545b185b
cyberpwn <danielmillst@gmail.com>
2019-06-24 22:25:11 -0400

    More configs

--- vdi.js
+++ vdi.js
@@ -4,6 +4,7 @@ function patch(mode)
     FS.copy(".minecraft/config/betterfoliage." + mode + ".cfg", ".minecraft/config/betterfoliage.cfg");
     FS.copy(".minecraft/config/Weather2/Particle." + mode + ".cfg", ".minecraft/config/Weather2/Particle.cfg");
     FS.copy(".minecraft/config/foamfix." + mode + ".cfg", ".minecraft/config/foamfix.cfg");
+    FS.copy(".minecraft/optionsof." + mode + ".txt", ".minecraft/optionsof.txt");
     FS.copy(".minecraft/options." + mode + ".txt", ".minecraft/options.txt");
     FS.copy(".minecraft/config/itlt." + mode + ".cfg", ".minecraft/config/itlt.cfg");
     FS.copy(".minecraft/config/itlt/icon." + mode + ".png", ".minecraft/config/itlt/icon.png");
