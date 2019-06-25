974fe9917a29567e4229f97636f4448966e42d73
cyberpwn <danielmillst@gmail.com>
2019-06-24 21:25:27 -0400

    Tweaks

--- vdi.js
+++ vdi.js
@@ -2,6 +2,7 @@ function patch(mode)
 {
     FS.copy("instance." + mode +".cfg", "instance.cfg");
     FS.copy(".minecraft/config/betterfoliage." + mode + ".cfg", ".minecraft/config/betterfoliage.cfg");
+    FS.copy(".minecraft/config/Weather2/Particle." + mode + ".cfg", ".minecraft/config/Weather2/Particle.cfg");
     FS.copy(".minecraft/config/foamfix." + mode + ".cfg", ".minecraft/config/foamfix.cfg");
 }
 
