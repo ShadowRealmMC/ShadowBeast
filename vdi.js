70cb4cb1afcac2fddc6f19e63059d776dfcc4503
cyberpwn <danielmillst@gmail.com>
2019-06-24 22:15:27 -0400

    cfg

--- vdi.js
+++ vdi.js
@@ -5,6 +5,11 @@ function patch(mode)
     FS.copy(".minecraft/config/Weather2/Particle." + mode + ".cfg", ".minecraft/config/Weather2/Particle.cfg");
     FS.copy(".minecraft/config/foamfix." + mode + ".cfg", ".minecraft/config/foamfix.cfg");
     FS.copy(".minecraft/options." + mode + ".txt", ".minecraft/options.txt");
+    FS.copy(".minecraft/config/itlt." + mode + ".cfg", ".minecraft/config/itlt.cfg");
+    FS.copy(".minecraft/config/itlt/icon." + mode + ".png", ".minecraft/config/itlt/icon.png");
+    FS.copy(".minecraft/config/smoothfont/smoothfont." + mode + ".cfg", ".minecraft/config/smoothfont/smoothfont.cfg");
+    FS.copy(".minecraft/resources/assets/fml/textures/gui/forge." + mode + ".gif", ".minecraft/resources/assets/fml/textures/gui/forge.gif");
+    FS.copy(".minecraft/resources/assets/fml/textures/gui/florb." + mode + ".gif", ".minecraft/resources/assets/fml/textures/gui/florb.gif");
 }
 
 patch(Fly.getParameter(0));
