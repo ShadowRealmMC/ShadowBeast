1f9305f2ac9d9de957f3036da99e2c25cb021e58
cyberpwn <danielmillst@gmail.com>
2019-06-26 18:15:01 -0400

    Apply patches

--- base/play-potato.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Configuration Mode: Potato
-java -jar Firefly.jar vdi.js potato
-cd ..
-cd ..
-echo Starting ShadowBeast...
-MultiMC -l "ShadowBeast"
-echo ShadowBeast is launching. This window will close automatically.
-TIMEOUT /T 10
