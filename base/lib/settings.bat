9993e51caa884ae41d7833eb63776bc585a4cc4e
cyberpwn <danielmillst@gmail.com>
2019-06-26 18:13:18 -0400

    Update

--- base/lib/settings.bat
+++ base/lib/settings.bat
@@ -6,5 +6,5 @@ set LAUNCHWRAPPER=net\minecraft\launchwrapper\%LAUNCHWRAPPERVERSION%\launchwrapp
 set FORGEJAR=1.12.2-14.23.5.2796-universal.jar
 
 rem can be changed by user
-set MAX_RAM=5G
+set MAX_RAM=8G
 set JAVA_PARAMETERS=-XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode  -XX:+CMSIncrementalPacing  -XX:NewRatio=16  -XX:SurvivorRatio=6  -XX:+AggressiveOpts  -XX:+UseStringDeduplication  -XX:+ExplicitGCInvokesConcurrent -XX:ParallelGCThreads=10 -Dfml.ignorePatchDiscrepancies=true  -Dfml.ignoreInvalidMinecraftCertificates=true  -XX:+UseFastAccessorMethods  -XX:+UseCompressedOops  -XX:+OptimizeStringConcat -XX:MaxTenuringThreshold=15 -XX:+UseAdaptiveGCBoundary -XX:+UseCodeCacheFlushing -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:+CMSClassUnloadingEnabled
--- base/vdi.js
+++ base/vdi.js
@@ -1,8 +1,11 @@
 function patch(mode)
 {
     FS.copy("instance." + mode +".cfg", "instance.cfg");
+    FS.copy(".minecraft/config/mobdismemberment." + mode + ".cfg", ".minecraft/config/mobdismemberment.cfg");
     FS.copy(".minecraft/config/betterfoliage." + mode + ".cfg", ".minecraft/config/betterfoliage.cfg");
+    FS.copy(".minecraft/config/chunkanimator." + mode + ".cfg", ".minecraft/config/chunkanimator.cfg");
     FS.copy(".minecraft/config/Weather2/Particle." + mode + ".cfg", ".minecraft/config/Weather2/Particle.cfg");
+    FS.copy(".minecraft/config/astralsorcery." + mode + ".cfg", ".minecraft/config/astralsorcery.cfg");
     FS.copy(".minecraft/config/foamfix." + mode + ".cfg", ".minecraft/config/foamfix.cfg");
     FS.copy(".minecraft/optionsof." + mode + ".txt", ".minecraft/optionsof.txt");
     FS.copy(".minecraft/options." + mode + ".txt", ".minecraft/options.txt");
@@ -11,6 +14,11 @@ function patch(mode)
     FS.copy(".minecraft/config/smoothfont/smoothfont." + mode + ".cfg", ".minecraft/config/smoothfont/smoothfont.cfg");
     FS.copy(".minecraft/resources/assets/fml/textures/gui/forge." + mode + ".gif", ".minecraft/resources/assets/fml/textures/gui/forge.gif");
     FS.copy(".minecraft/resources/assets/fml/textures/gui/florb." + mode + ".gif", ".minecraft/resources/assets/fml/textures/gui/florb.gif");
+
+    if(mode == "ultra")
+    {
+        FS.copy(".minecraft/optionsshaders.ultra.txt", ".minecraft/optionsshaders.txt");
+    }
 }
 
 patch(Fly.getParameter(0));
